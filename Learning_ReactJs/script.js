14e95dc8b104e9d94a3cff2cbf01edc232a04d83
Rudra swain <rudrasankar96@gmail.com>
2025-08-19 21:36:26 +0530

    Adding js function for count operation

---
+++ Learning_ReactJs/script.js
@@ -0,0 +1,16 @@
+const firstBasket = document.querySelector(`.basket-1 span`)
+const secondBasket = document.querySelector(".basket-2 span")
+
+const rightArrow = document.querySelector(".right-arrow")
+const leftArrow = document.querySelector(".left-arrow")
+
+const totalApples = 10;
+let secondBasketAppleCount = 0;
+let firstBasketAppleCount = totalApples - secondBasketAppleCount;
+
+firstBasket.innerText= firstBasketAppleCount;
+secondBasket.innerText = secondBasketAppleCount;
+
+rightArrow.addEventListener("click", ()=>{
+
+})
